db3c6287f24bceb6e63796f3303c840a081851e6
Dimm377 <dimaswijay2345@gmail.com>
2025-12-18 09:52:15 +0700

    final

--- Movie-Web/src/App.jsx
+++ Movie-Web/src/App.jsx
@@ -79,13 +79,13 @@ const App = () => {
         : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
       const response = await fetch(endpoint, API_OPTIONS);
       if (!response.ok) {
-        throw new Error("Network response was not ok");
+        throw new Error("Network respon mati");
       }
       const data = await response.json();
       if (data.results) {
         console.log("Fetched movies:", data.results);
       } else {
-        setErrorMessage("No movies found.");
+        setErrorMessage("gada movie yang ditemukan");
         setMovieList([]);
         return;
       }
@@ -157,7 +157,7 @@ const App = () => {
                 viewBox="0 0 24 24"
                 fill="currentColor"
               >
-                <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
+                <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
               </svg>
             </a>
 
@@ -166,14 +166,16 @@ const App = () => {
               data-aos="fade-up"
               data-aos-delay="200"
             >
-              <span className="text-red-400 underline">Dimm's</span> Movie Interface
+              <span className="text-red-400 underline">Dimm's</span> Movie
+              Interface
             </h1>
             <p
               className="text-light-200 text-xl mt-3 text-center"
               data-aos="fade-up"
               data-aos-delay="300"
             >
-              <span className="text-red-400 font-semibold">Up to date</span> movies info only in here
+              <span className="text-red-400 font-semibold">Up to date</span>{" "}
+              movies info only in here
             </p>
             <div data-aos="fade-up" data-aos-delay="400">
               <Search searchWeb={searchWeb} setSearchWeb={setSearchWeb} />
@@ -208,9 +210,7 @@ const App = () => {
 
           {/* All Movies Section */}
           <section className="all-movies">
-            <h2 data-aos="fade-right">
-              All Movies
-            </h2>
+            <h2 data-aos="fade-right">All Movies</h2>
             {isLoading ? (
               <Spinner />
             ) : errorMessage ? (
